4c378f7d59b2b3ca071b6fc4747fdb81f85ef7ec
Ash Berlin <ash_github@firemirror.com>
2014-07-24 12:02:48 +0100

    Fix the test runner

--- custom-test/run.sh
+++ custom-test/run.sh
@@ -4,4 +4,7 @@ set -e
 
 dir="$(dirname $0)"
 
+# We need the port logstash indexer is listening on for our tests.
+service logstash-indexer stop
+
 java -jar /usr/src/packages/logstash-1.2.2-flatjar.jar rspec "$dir/filter_test_spec.rb"
